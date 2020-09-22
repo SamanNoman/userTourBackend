3a4f862c46747d4eaf3ea08a9993261be93e41c1
Saman <Saman@INNOMINDSBANG.COM>
2020-09-21 22:49:22 -0700

    add changes to deploayment

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ mongoose
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.sgf1k.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
   )
   .then(() => {
-    app.listen(8000);
+    app.listen(process.env.PORT || 8000);
   })
   .catch((err) => {
     console.log(err);

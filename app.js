b4a1e1a79bb0a62860b1af72025dc3445ab77b07
Saman <Saman@INNOMINDSBANG.COM>
2020-09-24 00:48:01 -0700

    backend app updated

--- app.js
+++ app.js
@@ -47,8 +47,9 @@ app.use((error, req, res, next) => {
 });
 
 mongoose
-  .connect(
-    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.sgf1k.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+  .connect(    
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.sgf1k.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+    { useNewUrlParser: true, useUnifiedTopology: true }
   )
   .then(() => {
     app.listen(process.env.PORT || 8000);
@@ -56,3 +57,6 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+
+  // k1lB0SpGQejLGefF
